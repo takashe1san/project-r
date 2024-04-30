0080a319d92a47750ad2ec3fe404600cc2026df7
khaled <takashe1san@gmail.com>
2024-04-30 11:40:21 +0300

    init Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Traits\FileManager;
+use App\Traits\SaveImage;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, FileManager, SaveImage;
 
      /**
      * The attributes that are mass assignable.
@@ -15,7 +17,9 @@ class Category extends Model
      * @var array<int, string>
      */
     protected $fillable = [
-        
+        'name',
+        'description',
+        'image',
     ];
 
     /**
