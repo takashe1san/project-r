7fb7733927552adc9db5e9830776468925100077
khaled <takashe1san@gmail.com>
2024-05-03 20:17:24 +0300

    show categories

--- resources/views/dashboard/categories/index.blade.php
+++ resources/views/dashboard/categories/index.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.dashboardlayout')
 
 @section('content')
-    
+    <table class="table table-striped">
+        <tr>CATEGORIES</tr>
+        <thead>
+            <tr>
+                <th>name</th>
+                <th>description</th>
+                <th>procedures</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($categories as $category)
+                <tr>
+                    <td>{{ $category->name }}</td>
+                    <td>{{ $category->description }}</td>
+                    <td></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
