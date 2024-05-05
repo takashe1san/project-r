901b69bc6e7fe98d8b56d1bc0739a3da19113c6e
khaled <takashe1san@gmail.com>
2024-05-05 10:20:41 +0300

    delete category modal

--- resources/views/dashboard/categories/index.blade.php
+++ resources/views/dashboard/categories/index.blade.php
@@ -5,7 +5,7 @@
         <strong>CATEGORIES</strong>
         <div>
             {{-- <a href="{{ route('category.add') }}" class="btn btn-success">add</a> --}}
-            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#myModal">
+            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#createModal">
                 add
             </button>
         </div>
@@ -15,6 +15,7 @@
             <tr>
                 <th>name</th>
                 <th>description</th>
+                <th>products</th>
                 <th>procedures</th>
             </tr>
         </thead>
@@ -23,8 +24,41 @@
                 <tr>
                     <td>{{ $category->name }}</td>
                     <td>{{ $category->description }}</td>
+                    <td></td>
                     <td>
-                        <a href="{{ route('category.destroy', ['category' => $category->id])}}" class="btn btn-danger">delete</a>
+                        <button type="button" class="btn btn-danger" data-toggle="modal"
+                            data-target="#deleteModal{{ $category->id }}">
+                            delete
+                        </button>
+                        <!-- The Modal -->
+                        <div class="modal" id="deleteModal{{ $category->id }}">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+
+                                    <!-- Modal Header -->
+                                    <div class="modal-header">
+                                        <h4 class="modal-title">DELETE CATEGORY</h4>
+                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    </div>
+                                    <!-- Modal body -->
+                                    <div class="modal-body">
+                                        <div class="row px-3">
+                                            Are you sure you want to delete the category?
+                                        </div>
+                                    </div>
+
+                                    <!-- Modal footer -->
+                                    <div class="modal-footer">
+                                        <a href="{{ route('category.destroy', ['category' => $category->id]) }}"
+                                            class="btn btn-danger mx-1">delete</a>
+
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                                    </div>
+                                    </form>
+
+                                </div>
+                            </div>
+                        </div>
                     </td>
                 </tr>
             @endforeach
@@ -32,7 +66,7 @@
     </table>
 
     <!-- The Modal -->
-    <div class="modal" id="myModal">
+    <div class="modal" id="createModal">
         <div class="modal-dialog">
             <div class="modal-content">
 
@@ -42,14 +76,15 @@
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
                 </div>
 
-                <form method="POST" action="{{ route('category.store')}}">
+                <form method="POST" action="{{ route('category.store') }}">
                     @csrf
                     <!-- Modal body -->
                     <div class="modal-body">
                         <div class="row">
                             <input type="text" class="form-control" id="name" placeholder="Enter name"
                                 name="name">
-                            <textarea name="description" class="form-control px-3" id="description" cols="30" rows="10" placeholder="Enter description"></textarea>
+                            <textarea name="description" class="form-control px-3" id="description" cols="30" rows="10"
+                                placeholder="Enter description"></textarea>
                         </div>
                     </div>
 
