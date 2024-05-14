626a5abbf9c90078789a9b2bd8be8c7f6976bca6
khaled <takashe1san@gmail.com>
2024-05-14 20:18:42 +0300

    .

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 
-Route::get('login', [AuthController::class, 'login'])->name('login');
-Route::post('login', [AuthController::class, 'authenticate'])->name('auth');
-Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+Route::group([
+    'controller' => AuthController::class,
+], function () {
+    Route::get('login', 'login')->name('login');
+    Route::post('login', 'authenticate')->name('auth');
+    Route::get('logout', 'logout')->name('logout');
+});
 
 Route::group([
     'prefix' => 'dashboard',
