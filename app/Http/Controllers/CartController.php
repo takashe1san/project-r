6721ee1e9fe696324ab3fa69d1f362404d01e40d
khaled <takashe1san@gmail.com>
2024-05-18 23:27:38 +0300

    .

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -48,7 +48,7 @@ class CartController extends Controller
                 return redirect()->back()->with(['m-color' => 'danger', 'message' => 'فشل حذف العنصر من السلة.', 'm-dir' => 'rtl']);
             }
         } else {
-            return redirect()->back()->with(['m-color' => 'danger', 'message' => 'لم يتم العثور على العنصر']);
+            return redirect()->back()->with(['m-color' => 'danger', 'message' => 'لم يتم العثور على العنصر', 'm-dir' => 'rtl']);
         }
     }
 }
