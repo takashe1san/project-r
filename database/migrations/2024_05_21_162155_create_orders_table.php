c1cfc5d835f446d6a320f4b907dbc584d1c5b61f
khaled <takashe1san@gmail.com>
2024-05-21 19:29:03 +0300

    orders table migration

--- database/migrations/2024_05_21_162155_create_orders_table.php
+++ database/migrations/2024_05_21_162155_create_orders_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('address');
+            $table->tinyInteger('status');
+            $table->string('cancelation_notes')->nullable();
+            $table->string('rejection_notes')->nullable();
+            $table->string('notes')->nullable();
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
